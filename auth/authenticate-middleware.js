82280e3145fae423aaf898711553a5ddc6e2ea17
Miley Wright <dishaliekwright@yahoo.com>
2020-02-21 17:10:48 -0500

    authenticate middleware

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -3,6 +3,23 @@
   before granting access to the next middleware/route handler
 */
 
+const secrets = require('../config/secrets');
+const jwt = require('jsonwebtoken');
+
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+
+    const token = req.headers.authorization;
+    console.log(token)
+       if (token) {
+        jwt.verify(token, secrets.jwtSecret, (err, decodedJwt) => {
+            if(err) {
+                res.status(401).json({message: 'shall not pass!'})
+            } else {
+                req.user = decodedJwt;
+                next();
+            }
+        })
+    } else {
+        res.status(400).json({message: 'Nope try again'})
+    }
 };
